59a42a8a144668eeab724130e565e655f39cdd6e
rokity <riccardo.amadio1@gmail.com>
2018-10-25 13:32:44 +0200

    get default branch

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(req.body['repository']);
+  console.log(req.body['repository']['default_branch']);
   res.end();
 });
 
