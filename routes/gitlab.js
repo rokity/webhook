7db7c0f38878b608a86d112bb6a842e476daeb71
rokity <riccardo.amadio1@gmail.com>
2018-10-29 21:20:07 +0100

    update webhook anda add gitlab

---
+++ routes/gitlab.js
@@ -0,0 +1,50 @@
+var express = require('express');
+var router = express.Router();
+var exec = require('child_process').exec;
+
+/* POST GITHUB */
+router.post('/', function (req, res, next) {
+
+  var branch = req.body['ref'].split('/')[2]
+  switch (branch) {
+    case "master":
+      {
+        new Promise(resolve => {
+          exec('cd /home/ubuntu/server-web-master-gitlab &&  git pull gitlab master && npm install && sudo systemctl restart server-web-master-gitlab.service',
+            (error, stdout, stderr) => {
+              console.log(`${stdout}`);
+              console.error(`${stderr}`);
+              if (error !== null) {
+                console.error(`exec error: ${error}`);
+              }
+              resolve()
+            });
+
+        }).then(val => res.end());
+        break;
+      }
+    case "dev":
+    {
+      new Promise(resolve => {
+        exec('cd /home/ubuntu/server-web-dev-gitlab &&  git pull gitlab dev &&  npm install && wsudo systemctl restart server-web-dev-gitlab.service',
+          (error, stdout, stderr) => {
+            console.log(`${stdout}`);
+            console.error(`${stderr}`);
+            if (error !== null) {
+              console.error(`exec error: ${error}`);
+            }
+            resolve()
+          });
+
+      }).then(val => res.end());
+      break;
+    }
+    default:
+      res.end();
+  }
+});
+
+
+
+
+module.exports = router;
